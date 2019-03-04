8fa01fd202747ab317e4cffe06b947fd367828bd
Banlim <sanga1563@naver.com>
2019-03-04 21:56:32 +0900

    modify test

--- test.java
+++ test.java
@@ -2,6 +2,7 @@
 public class test {
 
 	public static void main(String[] args) {
+		/* Queue test
 		Queue q = new Queue(5);
 		q.enqueue(1);
 		q.enqueue(2);
@@ -20,6 +21,61 @@ public class test {
 		System.out.println("peek : " + q.peek());
 		q.dequeue();
 		System.out.println("size : " + q.size());
+		*/
+		
+		/* LinkedList test
+		LinkedList l = new LinkedList();
+		l.removeLast();
+		l.addFirst(1);
+		l.addFirst(2);
+		l.addLast(3);
+		l.addLast(7);
+		l.removeLast();
+		l.dump();
+		l.search(1);
+		l.printCurrentNode();
+		l.currentRemove();
+		l.dump();
+		l.removeFirst();
+		l.addLast(4);
+		l.dump();
+		l.currentRemove();
+		System.out.println("dump!");
+		l.dump();
+		l.addFirst(5);
+		l.printCurrentNode();
+		l.addFirst(6);
+		l.dump();
+		l.removeFirst();
+		//System.out.println(l.printSize());
+		l.dump();
+		l.clear();
+		System.out.println(l.printSize());
+		l.dump();
+		*/
+		
+		/* Buuble Sort test
+		BubbleSort bs = new BubbleSort();
+		int [] arr = {5, 9, 4, 1, 3};
+		bs.arr_int_Sort(arr);
+		bs.arr_sort_print();
+		*/
+		
+		/* Quick Sort test
+		int [] arr = {5, 3, 8, 4, 9, 1, 6, 2, 7};
+		QuickSort qs = new QuickSort(arr);
+		qs.int_sort(arr, 0, arr.length-1);
+		qs.print_quick_sort();
+		*/ 
+		
+		/* Merge Sort test
+		int [] arr = {21, 10, 12, 20, 25, 13, 15, 22};
+		MergeSort ms = new MergeSort(arr);
+		ms.print_sort_arr();
+		*/
+		
+		
+		
 
 	}
 
